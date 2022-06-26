a88b1ddd876f1eab246a83a1a614bfd9a91bc562
Eddie <eddie@planit.ai>
2022-06-26 20:24:53 +0900

    add : connection 활용법 샘플 추가

---
+++ dags/14_connection.py
@@ -0,0 +1,49 @@
+from datetime import datetime
+from time import sleep
+import json
+
+from airflow import DAG
+from airflow.providers.sqlite.operators.sqlite import SqliteOperator
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+
+default_arg = {
+    'owner': 'eddie',
+    'start_date' : datetime(2022,6,19),
+}
+
+with DAG(
+    dag_id='ex_connections',
+    schedule_interval='@daily',
+    default_args=default_arg,
+    tags=['test', 'connection sample'],
+    catchup=False
+) as dag:
+
+    def dump():
+        sleep(3)
+
+    start = PythonOperator(
+        task_id='start',
+        python_callable=dump
+    )
+
+    # 기본적으로 Http호출하는 Operator
+    extract_gorest = SimpleHttpOperator(
+        task_id='extract_gorest',
+        http_conn_id='GoRest',
+        endpoint='v2/users',
+        method='GET',
+        response_filter=lambda res: json.loads(res.text),
+        log_response=True
+    )
+
+    end = PythonOperator(
+        task_id='end',
+        python_callable=dump
+    )
+
+    start >> extract_gorest >> end
+
+
+
